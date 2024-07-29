4e7bf8c3e0587e3d09f2358c5735348ae9c70250
Davi Mathais <davimathais@gmail.com>
2024-07-29 01:27:56 -0300

    Fix: "Our Product" Section

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -2,22 +2,28 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import ProductCard from './ProductCard'
 
-const ProductList = () => {
-  const [products, setProducts] = useState([])
+interface ProductListProps {
+  limit?: number
+}
+
+const ProductList: React.FC<ProductListProps> = ({ limit }) => {
+  const [products, setProducts] = useState<any[]>([])
 
   useEffect(() => {
     axios.get('http://localhost:3000/products')
       .then(response => {
-        setProducts(response.data);
+        setProducts(response.data)
       })
       .catch(error => {
         console.error('There was an error fetching the data!', error)
       })
   }, [])
 
+  const displayedProducts = limit ? products.slice(0, limit) : products;
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {products.map(product => (
+      {displayedProducts.map(product => (
         <ProductCard key={product.id} product={product} />
       ))}
     </div>
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -9,22 +9,22 @@ import CardImg3 from "../assets/CardImgHome3.svg"
 import ProductList from "../components/ProductList"
 
 const Home = () => {
-  return(
+  return (
     <>
       <Header />
       <BannerHome />
       <div className="text-center mt-14">
-      <h3 className="font-bold text-3xl text-[#333333]">Browse The Range</h3>
-      <p className="mt-2 text-[#666666]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <h3 className="font-bold text-3xl text-[#333333]">Browse The Range</h3>
+        <p className="mt-2 text-[#666666]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </div>
-      <div className="grid grid-flow-col text-center justify-center gap-5 mt-8">    
-      <BrowseHome CardImg={CardImg1} Title={<h1>Dining</h1>} />
-      <BrowseHome CardImg={CardImg2} Title={<h1>Living</h1>} />
-      <BrowseHome CardImg={CardImg3} Title={<h1>Bedroom</h1>} />
+      <div className="grid grid-flow-col text-center justify-center gap-5 mt-8">
+        <BrowseHome CardImg={CardImg1} Title={<h1>Dining</h1>} />
+        <BrowseHome CardImg={CardImg2} Title={<h1>Living</h1>} />
+        <BrowseHome CardImg={CardImg3} Title={<h1>Bedroom</h1>} />
       </div>
       <div className="w-4/5 m-auto">
-      <h2 className="font-bold text-center text-2xl mb-2 mt-12">Our Products</h2>
-      <ProductList />
+        <h2 className="font-bold text-center text-2xl mb-2 mt-12">Our Products</h2>
+        <ProductList limit={8} />
       </div>
       <ShareHome />
       <Footer />
